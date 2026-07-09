09af92848690c91791d3ef8beb0d8c34014bc4f0
navy1634 <kuni.saysis.009@gmail.com>
2026-07-09 21:30:07 +0900

    初期設定

---
+++ README.md
@@ -0,0 +1,5 @@
+# nextjsで遊んでみる
+
+- 最新のエコシステム達
+- Auth.js
+- markdown レンダリング
---
+++ src/components/Header/header.stories.tsx
@@ -0,0 +1,7 @@
+import type { Meta } from "@storybook/nextjs-vite";
+import Header from "@/components/Header/header";
+
+const meta = {
+  component: Header,
+} satisfies Meta<typeof Header>;
+export default meta;
